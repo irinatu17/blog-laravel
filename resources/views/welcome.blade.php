806592346ad26ec47a9ca3bbba4d307cf75b9068
irinatu17 <ira_tushina@mail.ru>
2021-04-22 11:10:43 +0100

    Links to custom css and js files added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,6 +19,13 @@
                 font-family: 'Nunito', sans-serif;
             }
         </style>
+
+        <!--Custom css file-->
+        <link rel="stylesheet" href="/public/app.css">
+
+        <!--Custom js file-->
+        <script src="/public/app.js"></script>
+
     </head>
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
